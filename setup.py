92c2a9765fb9baa598062790de3df8b17293a0c1
Jamie <jamiebloomfield8@gmail.com>
2024-01-01 05:35:05 +0000

    Move details to pyproject.toml

--- setup.py
+++ setup.py
@@ -2,35 +2,8 @@ from setuptools import setup
 import os
 
 if "srcpy-version" in os.environ:
-    version = os.environ["srcpy-version"]
+    __version__ = os.environ["srcpy-version"]
 
-# version = "v0.0.0" # For non-automated builds
+# __version__ = "v0.0.0-alpha" # For non-automated builds
 
-with open("README.md") as f:
-    desc = f.read()
-
-setup(
-    name='speedruncompy',
-    version=version,
-    author="Jamie",
-    author_email="jamiebloomfield8@gmail.com",
-    description="A wrapper for speedrun.com's new v2 API, as used by their new site",
-    long_description=desc,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ManicJamie/speedruncompy",
-    project_urls={
-        "Bug tracker": "https://github.com/ManicJamie/speedruncompy/issues"
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ],
-    python_requires=">=3.11",
-    packages=['speedruncompy'],
-    package_data={'': ['.version']},
-    include_package_data=True,
-    install_requires=[
-        'requests'
-    ],
-)
+setup(version=__version__)
