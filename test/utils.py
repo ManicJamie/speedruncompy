da301065e051d994d2cc255e8b8ee08227be7a2d
Jamie <jamie@manicjamie.com>
2026-01-10 00:48:35 +0000

    Fix check_model_coverage output
    dict.items() strikes again :(

--- test/utils.py
+++ test/utils.py
@@ -8,7 +8,7 @@ def check_model_coverage(m: SpeedrunModel):
     """Assert that a model has all its members"""
     if m.model_extra is None:
         raise Exception("Tests must be run with config.extra == 'allow'!")
-    assert len(m.model_extra) == 0, f"Model {type(m)} is missing provided keys: {[f'{k} = {v}' for k, v in m.model_extra]}"
+    assert len(m.model_extra) == 0, f"Model {type(m)} is missing provided keys: {[f'{k} = {v}' for k, v in m.model_extra.items()]}"
 
 def check_pages(pages: dict[int, SrcompyModel]):
     for p, model in pages.items():
